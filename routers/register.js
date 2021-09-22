d7a28cfaede189c79bb4fa89fec2a5134628013d
dilanzib <dilozibari@gmail.com>
2021-09-22 11:11:49 +0300

    Laddat ner mongoose och dotenv

---
+++ routers/register.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const router = express.Router()
+const Register = require("../models/registerModel")   //behöver register data modell
+
+
+
+console.log("Register funkar!")
+
+
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -3,8 +3,14 @@ const app = express();
 const PORT = process.env.PORT || 3030;
 
 // Testa att detta funkar, ersätt sedan med egen kod
-app.get('/test', (req, res) => {
-    res.json("Det fungerar!")
+app.get('/register', (req, res) => {
+    res.json("Register funkar")
 })
+
+
+//Vi importerar Register route-modul 
+const registerRouter = require('./routers/register')
+app.use('/register', registerRouter)
+
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
