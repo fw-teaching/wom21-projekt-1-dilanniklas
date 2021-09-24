3b1e013e36386cf8a3239d398c035f710da08f9d
dilanzib <dilozibari@gmail.com>
2021-09-24 08:10:36 +0000

    Register module and router

--- routers/register.js
+++ routers/register.js
@@ -1,10 +1,31 @@
 const express = require("express")
 const router = express.Router()
-const Register = require("../models/registerModel")   //behöver register data modell
+const bcrypt = require("bcrypt")
+const Register = require("../models/registerModel")   //behöver register data modellen
 
+console.log("Register läses in")
 
+//Skapa en användare mha POST
+router.post('/', async (req, res) =>{
 
-console.log("branch dilan merged into main branch")
+    const hashedPassword = await bcrypt.hash(req.body.password, 12)   //tar password från bodyn och hashar det
+
+    try {
+        const register = new Register({  //tar namn,efternamn,emailen från bodyn från REST_clienten
+            name: req.body.name,
+            surname: req.body.surname,
+            email: req.body.email,     
+            password: hashedPassword
+        })
+
+        const newRegister = await register.save()
+        res.status(201).send(newRegister)
+
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
 
 
 
--- server.js
+++ server.js
@@ -1,10 +1,21 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3030;
+require('dotenv').config()   //läser in .env filen 
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 3030
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DB_URL)  
+const db = mongoose.connection    //skapar databas connection
+db.on('error', (error) => console.log(error))
+db.once('open', () => console.log('Connected to DB'))
+
+
+app.use(express.json())  //API:n ska ta emot JSON-format 
 
 // Testa att detta funkar, ersätt sedan med egen kod
-app.get('/register', (req, res) => {
-    res.json("Register funkar")
+app.get('/', (req, res) => {
+    res.json({message: "Serverjs funkar"})
 })
 
 
