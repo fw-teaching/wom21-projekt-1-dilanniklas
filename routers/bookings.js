6ae66d618965843dd1e71d9866f7b200151322bd
Kraakan <60356996+Kraakan@users.noreply.github.com>
2021-10-01 21:01:46 +0300

    Added function that checks for overlapping bookings

--- routers/bookings.js
+++ routers/bookings.js
@@ -1,6 +1,5 @@
 const express = require("express")
 const router = express.Router()
-const jwt = require("jsonwebtoken")
 const Booking = require("../models/bookingsModel")
 const Cabin = require("../models/cabinsModel")
 const authorize = require('../middleware/authorize')
@@ -8,21 +7,50 @@ const authorize = require('../middleware/authorize')
 // middleware för authorisering
 router.use(authorize)
 
+// funktion för att kolla om stugan är ledig
+async function checkBooking(cabin_id, startDate, endDate){
+    const b = await Booking.find({cabinId: cabin_id})
+    if (b.length == 0 || !b) return null
+    var overBookings = []
+    dateStart = new Date(startDate)
+    dateEnd = new Date(endDate)
+    for (i=0;i<b.length;i++)
+    {
+        let ds = new Date(b[i]['arrivalDate'])
+        let de = new Date(b[i]['departureDate'])
+        // Jag förmodar här att bokningarna alltid slutar senare än de börjar, så att man bara behöver jämföra
+        // den ena periodens slutdatum med den andras startdatum för att kolla om de överlappar
+        if ( !(dateEnd < ds) || (dateStart > de) )
+        {
+            overBookings.push({"arrivalDate" : ds, "departureDate" : de})
+        }
+    }
+    if (overBookings.length == 0) return null
+    return overBookings
+}
+
 // Skapa en bokning med POST
 router.post('/:cabin_id', async (req, res) =>{
-    
+    // TODO: Kolla om tiden är ledig
     try
     {
-        const booking = new Booking({
-            "cabinId": req.params.cabin_id,
-            "lodger": req.authUser.email,
-            "arrivalDate": req.body.arrivalDate,
-            "departureDate": req.body.departureDate
-        })
-
-        const newBooking = await booking.save()
-
-        res.status(201).send(newBooking)
+        const bookingCheck = await checkBooking(req.params.cabin_id, req.body.arrivalDate, req.body.departureDate);
+        if (bookingCheck==null)
+        {
+            const booking = new Booking({
+                "cabinId": req.params.cabin_id,
+                "lodger": req.authUser.email,
+                "arrivalDate": req.body.arrivalDate,
+                "departureDate": req.body.departureDate
+            })
+            const newBooking = await booking.save()
+            res.status(201).send(newBooking)
+        }
+        else 
+        {
+            console.log(bookingCheck)
+            res.status(201).json(bookingCheck)
+        }
     }
     catch (error)
     {
